7d7e3118eafde5bbcda9378b9ca12bf38f32f2b2
tuomina9 <akseli.tuominen@aalto.fi>
2023-12-31 13:50:54 +0200

    Some serious reworsk

--- ScattegoriesFront/src/App.jsx
+++ ScattegoriesFront/src/App.jsx
@@ -1,15 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext} from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, useParams } from 'react-router-dom';
 import './App.css';
-import alarmSound from './alarm.wav';
+import alarmSound from './assets/clock.mp3';
+import categories from './assets/ScattegoriesCategories.txt'
 
-function Game() {
-  const { id } = useParams();
-  const [count, setCount] = useState(60);
+function MainMenu() {
   const [number, setNumber] = useState();
-  const [isRunning, setIsRunning] = useState(false);
+  const [categoriesList, setCategoriesList] = useState([]);
+  const [count, setCount] = useState(0);
   const [round, setRound] = useState(0);
-  const audio = new Audio(alarmSound);
+  const [isRunning, setIsRunning] = useState(false);
+  const [audio, setAudio] = useState(null);
+
+useEffect(() => {
+  const audioObj = new Audio(alarmSound);
+  setAudio(audioObj);
+}, []);
 
   useEffect(() => {
     if (isRunning && count > 0) {
@@ -17,7 +23,6 @@ function Game() {
       return () => clearTimeout(timer);
     } else if (count === 0) {
       setIsRunning(false);
-      playSound();
     }
   }, [count, isRunning]);
 
@@ -27,22 +32,42 @@ function Game() {
     }
   }, [isRunning]);
 
-  function playSound() {
-    audio.play();
-    audio.volume = 0.2;
-    setTimeout(() => audio.pause(), 4000);
-  }
+  const cpr = 12
 
+  useEffect(() => {
+    // Fetch categories only when the component mounts
+    fetch(categories)
+      .then(response => response.text())
+      .then(data => {
+        const categories = data.split('\n').filter(category => category)
+        setCategoriesList(categories)
+      });
+  }, [])
+  function startGame(){
+    setIsRunning(true)
+    setCount(60)
+    if (audio) {
+      audio.play().catch(error => console.log(error));
+    }
+  }
   function randomChar() {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    setNumber(chars.charAt(Math.floor(Math.random() * chars.length)));
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    setNumber(chars.charAt(Math.floor(Math.random() * chars.length)))
   }
-
-  const rotation = (count / 60) * 360;
+  function restart(){
+    setCount(60)
+    setIsRunning(false)
+    setRound(0)
+    if (audio) {
+      audio.pause();
+      audio.currentTime = 0;  // Optional: reset audio to start
+    }
+  }
+  const rotation = (count / 60) * 360
 
   return (
     <div>
-      <h1>Game {id} - Round: {round}</h1>
+      <h1> Round: {round}</h1>
       <h1>{count > 0 ? `Time remaining: ${count} seconds` : 'Time is up!'}</h1>
       <svg width="200" height="200">
         <circle cx="100" cy="100" r="80" stroke="black" strokeWidth="4" fill="white" />
@@ -50,39 +75,50 @@ function Game() {
       </svg>
       {!isRunning && (
         <div>
-          <button onClick={() => { setCount(60); setIsRunning(true); }}>Start</button>
+           <button onClick={startGame}>Start</button>
         </div>
       )}
       {!isRunning && (
         <div>
-          <button onClick={() => randomChar()}>Random Char</button>
-        </div>
+          <button onClick={() => randomChar()}>Roll</button>
+        </div>       
       )}
+      <div>
+        <button onClick = {() => restart()}>Restart</button>
+      </div>
       <h1>{number}</h1>
+      {categoriesList.slice((round-1) * cpr,round*cpr).map((category, index) => (
+        <div key={index}>
+          <label>{category}</label>
+          <input type="text" disabled={count === 0} />
+        </div>
+      ))}
     </div>
   );
 }
+function Tutorial() {
+  return(
+    <div>
+      <h1>Scattegories rules</h1>
+     <ul>
+        <h2> Press Roll, you will see a letter</h2>
+        <h2> The goal of the game is to give an example of each of the categories presented starting with the spesific letter</h2>
+        <h2> Try to come up with unique answers, because duplicate answers give no points(only in multiplayer)</h2>
+        <h2> You have 60 seconds to complete the task</h2>
+        <h2> Press Start to start the timer</h2>
+        <h2> Press Restart to restart the game</h2>
+      </ul>
+    </div>
+  )
+}
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/game/:id" element={<h1>HEllo</h1>} />
-        <Route
-          path="/game"
-          element={
-            <div>
-              <h1>Welcome to the Game Hub</h1>
-              <p>Click on the links below to start a game:</p>
-              <ul>
-                <li><Link to="/game/1">Game 1</Link></li>
-                <li><Link to="/game/2">Game 2</Link></li>
-                <li><Link to="/game/3">Game 3</Link></li>
-              </ul>
-            </div>
-          }
-        />
-        <Route path="/" element={<Game />} />
+      <Route path="/tutorial/" element={<Tutorial />} /> 
+        <Route path="/" element={<MainMenu />} />
       </Routes>
     </Router>
   );
