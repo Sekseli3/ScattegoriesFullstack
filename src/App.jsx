fe373f8309fb4fb1bcf0df67e48ebfadcda22529
tuomina9 <akseli.tuominen@aalto.fi>
2024-01-18 11:16:10 +0200

    Edited file structure

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import './App.css'
+import MainMenu from './MainMenu'
+import Tutorial from './tutorial' // Fix the file name casing
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+      <Route path="/tutorial/" element={<Tutorial />} /> 
+        <Route path="/" element={<MainMenu />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
---
+++ src/MainMenu.jsx
@@ -0,0 +1,130 @@
+import React, { useState, useEffect} from 'react'
+import './App.css'
+import alarmSound from './assets/clock.mp3'
+import explosion from './assets/explosion.mp3'
+import categories from './assets/ScattegoriesCategories.txt'
+
+function MainMenu() {
+    const [number, setNumber] = useState()
+    const [categoriesList, setCategoriesList] = useState([])
+    const [count, setCount] = useState(0)
+    const [round, setRound] = useState(0)
+    const [isRunning, setIsRunning] = useState(false)
+    const [explosionAudio, setExplosionAudio] = useState(null);
+
+    const [audio, setAudio] = useState(null)
+    const [inputs, setInputs] = useState(Array(categoriesList.length).fill(''))
+  
+  useEffect(() => {
+    const audioObj = new Audio(alarmSound)
+    setAudio(audioObj)
+  }, [])
+
+  useEffect(() => {
+    const explosionAudioObj = new Audio(explosion);
+    setExplosionAudio(explosionAudioObj);
+  }, []);
+  
+useEffect(() => {
+  if (isRunning && count > 0) {
+    const timer = setTimeout(() => setCount(count - 1), 1000);
+    return () => clearTimeout(timer);
+  } else if (count === 0) {
+    if (explosionAudio) {
+      explosionAudio.play();
+    }
+    setIsRunning(false);
+  }
+}, [count, isRunning, explosionAudio]);
+  
+    useEffect(() => {
+      if (isRunning) {
+        setRound(round + 1)
+      }
+    }, [isRunning])
+  
+    const cpr = 12
+  
+    useEffect(() => {
+      // Fetch categories only when the component mounts
+      fetch(categories)
+        .then(response => response.text())
+        .then(data => {
+          const categories = data.split('\n').filter(category => category)
+          setCategoriesList(categories)
+        });
+    }, [])
+  
+    const startGame = () => {
+      if(number === undefined){
+        alert("Press Roll to get a letter")
+        return
+      }
+      setIsRunning(true);
+      setCount(60);
+      setInputs(Array(categoriesList.length).fill(''))
+      if (audio) {
+        audio.play().catch(error => console.log(error))
+      }
+    }
+  
+    function randomChar() {
+      const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      setNumber(chars.charAt(Math.floor(Math.random() * chars.length)))
+    }
+    function restart(){
+      setCount(60)
+      setIsRunning(false)
+      setRound(0)
+      if (audio) {
+        audio.pause()
+        audio.currentTime = 0;  // Optional: reset audio to start
+      }
+    }
+    const rotation = (count / 60) * 360
+  
+    return (
+      <div>
+        <h1> Round: {round}</h1>
+        <h1>{count > 0 ? `Time remaining: ${count} seconds` : 'Time is up!'}</h1>
+        <svg width="200" height="200">
+          <circle cx="100" cy="100" r="80" stroke="black" strokeWidth="4" fill="white" />
+          <line x1="100" y1="100" x2="100" y2="20" stroke="black" strokeWidth="4" transform={`rotate(${-rotation} 100 100)`} />
+        </svg>
+        {!isRunning && (
+          <div>
+             <button onClick={startGame}>Start</button>
+          </div>
+        )}
+        {!isRunning && (
+          <div>
+            <button onClick={() => randomChar()}>Roll</button>
+          </div>       
+        )}
+        <div>
+          <button onClick = {() => restart()}>Restart</button>
+        </div>
+        <h1>{number}</h1>
+    <div className="form-container">
+    <div className="form-group">
+        {categoriesList.slice((round-1) * cpr,round*cpr).map((category, index) => (
+      <div key={index} className="input-group">
+      <label className="category-lavel">{category  }</label>
+      <input className="category-input"
+        type="text" 
+        disabled={count === 0} 
+        value={inputs[index]} 
+        onChange={event => {
+          const newInputs = [...inputs]
+          newInputs[index] = event.target.value
+          setInputs(newInputs)
+        }}
+      />
+    </div>
+  ))}
+  </div>
+  </div>
+      </div>
+    );
+}
+export default MainMenu;
