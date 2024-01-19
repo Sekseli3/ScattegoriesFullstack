eedc2dbc798a16dea1509c579cdb173b7f5b92a6
tuomina9 <akseli.tuominen@aalto.fi>
2024-01-19 09:22:28 +0200

    Small fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { HashRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
 import MainMenu from './MainMenu'
 import Tutorial from './tutorial' // Fix the file name casing
